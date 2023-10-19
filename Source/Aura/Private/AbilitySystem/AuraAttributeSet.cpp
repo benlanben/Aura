d3500cf3954d00a47e86e28113033a9d276ad239
benlanben <samtim93@gmail.com>
2023-10-19 23:48:49 +0300

    Init Ability Actor Info

---
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -0,0 +1,5 @@
+// Copyright Benlan.
+
+
+#include "AbilitySystem/AuraAttributeSet.h"
+
--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -13,7 +13,11 @@ UCLASS()
 class AURA_API AAuraCharacter : public AAuraCharacterBase
 {
 	GENERATED_BODY()
+
+	void InitAbilityActorInfo();
+	
 public:
 	AAuraCharacter();
-	
+	virtual void PossessedBy(AController* NewController) override;
+	virtual void OnRep_PlayerState() override;
 };
